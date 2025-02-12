5a3a0fc0bfa6883f79dbdba6bce6ccdc473818df
Z3533617587 <3533617587@qq.com>
2025-02-12 09:31:40 +0800

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1,1 +1,13 @@
+const navbar = document.querySelector('.navbar');
+let timer;
+
+window.addEventListener('mousemove', () => {
+    if (navbar.style.top === '-50px') {
+        navbar.style.top = '0'; // 显示导航栏
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        navbar.style.top = '-50px'; // 隐藏导航栏
+    }, 3000); // 3秒后隐藏导航栏
+});
 
