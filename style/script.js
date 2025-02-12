fc563c8c167020c8c66da0deb7800b549137531b
Z3533617587 <3533617587@qq.com>
2025-02-12 09:43:37 +0800

    Update script.js

--- style/script.js
+++ style/script.js
@@ -11,3 +11,19 @@ window.addEventListener('mousemove', () => {
     }, 3000); // 3秒后隐藏导航栏
 });
 
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+
+function moveSlide(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+    const offset = -currentIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+
